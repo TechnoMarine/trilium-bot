b330fc8dae584f67db241de2dec96bb43e7b8a40
Vladislav <stelmistery@yandex.ru>
2024-03-12 00:46:39 +0300

    fix axios request to trilium

--- src/trilium/trilium.service.ts
+++ src/trilium/trilium.service.ts
@@ -2,28 +2,37 @@ import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ILibrabyReponse } from './trilium.interaces';
 import axios from 'axios';
+import { AxiosInstance } from 'axios';
 
 @Injectable()
 export class TriliumService {
   logger = new Logger('Trillium service');
-  private readonly gi: string;
   private readonly bookStorageId: string;
   private readonly notesParam = '/notes';
+  private readonly triliumUrl: string;
+  private readonly triliumApiKey: string;
+  private triliumPreRequest: AxiosInstance;
 
   constructor(private readonly configService: ConfigService) {
     this.triliumUrl = this.configService.get('TRILIUM_API_URL');
     this.bookStorageId = this.configService.get('BOOK_STORAGE_ID');
+    this.triliumApiKey = this.configService.get('TRILIUM_API_KEY');
+    this.triliumPreRequest = axios.create({
+      baseURL: new URL(this.triliumUrl).href,
+      timeout: 2000,
+      headers: {
+        Authorization: this.triliumApiKey,
+      },
+    });
   }
 
   async getBookList() {
-    const url = new URL(
+    const response = await this.triliumPreRequest.get(
       this.notesParam + '/' + this.bookStorageId,
-      this.triliumUrl,
+      { responseType: 'json' },
     );
-    console.log(url.href);
-    const response = await axios.get(url.href, { responseType: 'json' });
     const data: ILibrabyReponse = response.data;
-    return await this.getBooksNameFromTrilium(data.childNoteIds);
+    await this.getBooksNameFromTrilium(data.childNoteIds);
   }
 
   private async getBooksNameFromTrilium(
@@ -31,10 +40,12 @@ export class TriliumService {
   ): Promise<Map<string, string>> {
     const names: Map<string, string> = new Map<string, string>([]);
     for (const id of ids) {
-      const url = this.triliumUrl + this.notesParam + '/' + id;
-      const response = await axios.get(url, {
-        responseType: 'json',
-      });
+      const response = await this.triliumPreRequest.get(
+        this.notesParam + '/' + id,
+        {
+          responseType: 'json',
+        },
+      );
       const data: ILibrabyReponse = response.data;
       names.set(data.title, id);
     }
